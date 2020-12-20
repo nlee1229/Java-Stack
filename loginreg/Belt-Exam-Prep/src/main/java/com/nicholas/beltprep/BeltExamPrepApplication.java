23ea8fb0d65baff156731b761f87e8e5b31a867d
nlee1229 <72540269+nlee1229@users.noreply.github.com>
2020-12-19 22:41:29 -0800

    .

---
+++ loginreg/Belt-Exam-Prep/src/main/java/com/nicholas/beltprep/BeltExamPrepApplication.java
@@ -0,0 +1,13 @@
+package com.nicholas.beltprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeltExamPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeltExamPrepApplication.class, args);
+	}
+
+}
---
+++ loginreg/Belt-Exam-Prep/src/test/java/com/nicholas/beltprep/BeltExamPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholas.beltprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeltExamPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
